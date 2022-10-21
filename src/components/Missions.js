b07b7637c5975702004876ad26b5a078ff168a95
raphaelbaere <raphaelbaere@id.uff.br>
2022-10-21 15:50:01 -0300

    Requisito 8 concluído

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
-        <h1>Sistema Solar</h1>
+        <Title headline="Missões" />
       </div>
     );
   }
