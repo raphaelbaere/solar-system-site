f3e4b4166ff27919c978fe54da3c7f2172655fcc
raphaelbaere <raphaelbaere@id.uff.br>
2022-10-21 15:45:54 -0300

    Lint fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import PlanetCard from './components/PlanetCard';
-import planets from './data/planets';
 
 class App extends React.Component {
   render() {
-    const planetsLists = planets.map((planet, index) => (
-      <PlanetCard key={ index } planetName={ planet.name } planetImage={ planet.image } />
-    ));
     return (
       <div>
         <Header />
