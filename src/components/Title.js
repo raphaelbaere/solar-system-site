8b7f0fb4af30696727434e65020c2e58c8ba9fa7
raphaelbaere <raphaelbaere@id.uff.br>
2022-10-21 14:54:47 -0300

    Requisito 3 concluído

---
+++ src/components/Title.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Title extends Component {
+  render() {
+    const { headline } = this.props;
+    return <h2>{headline}</h2>;
+  }
+}
+
+Title.propTypes = {
+  headline: PropTypes.string,
+};
+
+Title.defaultProps = {
+  headline: 'Não foi encontrado HEADLINE',
+};
+
+export default Title;
