3ae200b8419ba74d4f518eb46bccf1aea05c2b19
raphaelbaere <raphaelbaere@id.uff.br>
2022-10-21 14:47:52 -0300

    Requisito 2 concluído

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h1>Sistema Solar</h1>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
